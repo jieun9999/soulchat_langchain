8df231e44f391102d42af72c1ef6556c66611e15
jieun9999 <kindjjee@gmail.com>
2025-03-26 15:03:04 +0000

    프롬프트 수정

--- langchain_route_chains.py
+++ langchain_route_chains.py
@@ -22,7 +22,7 @@ llm = HuggingFacePipeline.from_model_id(
     model_id="NCSOFT/Llama-VARCO-8B-Instruct",
     task="text-generation",
     pipeline_kwargs=dict(
-        max_new_tokens=100,  # 2문장, 총 길이 30자 이내
+        max_new_tokens=50,  # 응답 길이를 50 토큰으로 제한
         do_sample=False,
         repetition_penalty=1.03,
         return_full_text=False,
@@ -37,7 +37,7 @@ chat_model = ChatHuggingFace(llm=llm)
 # 2. 입력값과 3개의 sub-chain 만들기
 ###############################################################################################
 # 체인을 실행하기 위해서는 query 키를 포함하는 딕셔너리 형태의 데이터를 전달해야 합니다.
-input_data = {"query": "나는 분노를 느껴. 세탁기 때문에 내 통장이 완전 텅 비어버렸어"}
+input_data = {"query": "나는 슬퍼. 청바지를 새로 샀는데, 다리가 짧아보여. 망했어."}
 
 empathy_description = textwrap.dedent(f"""
     You are the user's lover. 
@@ -50,9 +50,9 @@ empathy_chain = ChatPromptTemplate.from_messages([
 ]) | chat_model | StrOutputParser()
 
 question_description = textwrap.dedent(f"""
-    You are the user's lover. 
-    Respond in a casual tone, using informal Korean as if speaking to a close lover.
-    Show interest in the user by asking specific questions about her situation.
+    You are the user's lover.         
+    Respond in a casual tone, using informal Korean as if speaking to a close lover. 
+    Ask specific questions about her situation.
 """)
 question_chain = ChatPromptTemplate.from_messages([
     SystemMessage(content=question_description),
@@ -62,7 +62,7 @@ question_chain = ChatPromptTemplate.from_messages([
 advice_description = textwrap.dedent(f"""
     You are the user's lover. 
     Respond in a casual tone, using informal Korean as if speaking to a close lover.
-    Provide the user with practical advice that suits her situation and can be helpful.
+    Doubt the user's thinking and suggest a better alternative.
 """)
 advice_chain = ChatPromptTemplate.from_messages([
     SystemMessage(content=advice_description),
@@ -87,7 +87,7 @@ def route(info):
 # 데이터가 오른쪽으로 체인을 따라 흐른다
 # 입력 데이터를 받아 route 함수를 호출하고, 적절한 체인을 선택합니다.
 #  이 체인은 Runnable 객체 또는 이를 처리할 수 있는 callable(함수, 람다 함수 등)을 기대합니다.
-data = {"topic": lambda x: "question", "query": lambda x: x["query"]} | RunnableLambda(
+data = {"topic": lambda x: "advice_chain", "query": lambda x: x["query"]} | RunnableLambda(
     route
 )
 # lambda x: "reaction"은 입력 데이터를 받아 "reaction" 문자열을 반환합니다.
